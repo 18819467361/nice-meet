6bd1eddad5c418818dd4d93d06e59bf700164440
yjy <yejiayuan163@163.com>
2019-04-10 21:52:15 +0800

    实现antd按需加载组件

--- config-overrides.js
+++ config-overrides.js
@@ -1,46 +1,25 @@
-// const {override, fixBabelImports, addLessLoader} = require('customize-cra');
-//
-// module.exports = override(
-//     fixBabelImports('import', {
-//         libraryName: 'antd',
-//         libraryDirectory: 'es',
-//         style: true,
-//     }),
-//     addLessLoader({
-//         javascriptEnabled: true,
-//         modifyVars: {'@primary-color': '#1DA57A'},
-//     }),
-// );
-// module.exports = function override(config, env) {
-//   // do stuff with the webpack config...
-//   console.log('config', config)
-//   config.module = {
-//     ...config.module,
-//     rules: [
-//       {
-//         test: /\.less$/,
-//         use: [
-//           require.resolve('style-loader'),
-//           require.resolve('css-loader'),
-//           {
-//             loader: require.resolve('less-loader'),
-//             options: {
-//               modifyVars: {'@primary-color': '#1DA57A'}
-//             }
-//           }
-//         ]
-//       }]
-//   }
-//
-//   return config
-// }
+/* config-overrides.js */
+const tsImportPluginFactory = require('ts-import-plugin')
+const { getLoader } = require("react-app-rewired");
 
-// const {override, fixBabelImports} = require('customize-cra')
-//
-// module.exports = override(
-//     fixBabelImports('import', {
-//       libraryName: 'antd',
-//       libraryDirectory: 'es',
-//       style: 'css'
-//     })
-// )
+module.exports = function override(config, env) {
+    const tsLoader = getLoader(
+        config.module.rules,
+        rule =>
+            rule.loader &&
+            typeof rule.loader === 'string' &&
+            rule.loader.includes('ts-loader')
+    );
+
+    tsLoader.options = {
+        getCustomTransformers: () => ({
+            before: [ tsImportPluginFactory({
+                libraryDirectory: 'es',
+                libraryName: 'antd',
+                style: 'css',
+            }) ]
+        })
+    };
+
+    return config;
+}
